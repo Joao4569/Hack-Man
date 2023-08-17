682da90fb109c15ccf607c9feb8276e6f62d65c9
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-17 19:05:34 +0100

    Added: add loadspritesheet atlas with maze-wall object and enemy objec
    also added for loop to start creating part of the maze just testing at the min

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,26 +1,57 @@
 import kaboom from "kaboom"
 
 const k = kaboom()
-
-k.loadSprite("enemy", "sprites/spritesheet.png",{
-	sliceX: 1,
-    sliceY: 1,
-    anims: {
-        run: {
-            from: 0,
-            to: 3,
-        },
-        jump: {
-            from: 3,
-            to: 3,
+// See #SpriteAtlasData type for format spec
+k.loadSpriteAtlas("sprites/spritesheet.png", {
+    "enemy": {
+        x: 18,
+        y: 84,
+        width: 133,
+        height: 14,
+        sliceX: 9,
+        anims: {
+            idle: { from: 0, to: 3 },
+            run: { from: 4, to: 7 },
+            hit: 8,
         },
     },
+	"maze-wall": {
+		x: 36,
+		y: 72,
+		width: 10,
+		height: 10
+	}
 })
 
 k.add([
 	k.pos(120, 80),
 	k.sprite("enemy"),
+	k.scale(3)
 ])
+
+k.add([
+	k.pos(10, 10),
+	k.sprite("maze-wall"),
+	k.scale(3)
+])
+
+for(let i = 0; i < 20; i++){
+	
+	k.add([
+		k.pos(i * 30, 10),
+		k.sprite("maze-wall"),
+		k.scale(3)
+	])
+	if(i == 19){
+		for(let j = 1;j < 20; j++){
+			k.add([
+				k.pos(i * 30, j * 30),
+				k.sprite("maze-wall"),
+				k.scale(3)
+			])
+		}
+	}
+}
 
 k.onClick(() => k.addKaboom(k.mousePos()))
 
