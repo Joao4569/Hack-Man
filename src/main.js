9af0f4f81c01c50f7d4121fe12d12ec48a57f475
rowanam <rowanamaas@gmail.com>
2023-08-18 13:33:44 +0000

    feat: add spritesheet and example code for adding sprites

--- src/main.js
+++ src/main.js
@@ -15,3 +15,34 @@ add([
     pos(80, 40),
     area(),
 ]);
+
+// Below - examples of how to add sprite objects to game from spritesheet
+// See #SpriteAtlasData type for format spec
+loadSpriteAtlas("src/sprites/spritesheet.png", {
+    "enemy": {
+        x: 18,
+        y: 84,
+        width: 15,
+        height: 15,
+    },
+	"maze-wall": {
+		x: 36,
+		y: 72,
+		width: 10,
+		height: 10
+	}
+})
+
+// CAN BE DELETED
+add([
+	pos(300, 300),
+	sprite("enemy"),
+	scale(3)
+])
+
+// CAN BE DELETED
+add([
+	pos(200, 100),
+	sprite("maze-wall"),
+	scale(3)
+])
