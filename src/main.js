50efb13bbba89fa5d8b428981e7c90902bc906c0
Jamie Bradford <jamiebradford@jamies-air.home>
2023-08-21 11:55:50 +0100

    fix music bug when starting new game

--- src/main.js
+++ src/main.js
@@ -187,7 +187,12 @@ scene("game", () => {
             height: 15,
         },
     })
+    loadSound("cruising", "assets/music/cruising.mp3")
 
+    const music = play("cruising", {
+        volume: 0.8,
+        loop: true
+    })
     // load  the player sprite sprite 
     loadSpriteAtlas("src/sprites/player-sprite.png", {
         "player": {
@@ -382,6 +387,7 @@ scene("game", () => {
         score.value -= 100
         score.text = "Score: " + score.value
         if (player.lives == 0) {
+            music.paused = true;
             go("over");
         }
         addKaboom(enemy.pos)
@@ -520,24 +526,13 @@ scene("game", () => {
 ////////////  Music has been commented out/disabled for development purposes!!!  ////////////////////
 
     // play a looping soundtrack
-    loadSound("cruising", "assets/music/cruising.mp3")
+    // loadSound("cruising", "assets/music/cruising.mp3")
 
-    const music = play("cruising", {
-        volume: 0.8,
-        loop: true
-    })
+    // const music = play("cruising", {
+    //     volume: 0.8,
+    //     loop: true
+    // })
 
-
-    //////////////////////////////// Exit Game Key Press ///////////////////////////////////////////////
-
-
-    // Delete this section!! For testing purposes only
-
-    onKeyPress("q", () => {
-        go("over")
-    })
-
-})
 
 
 ////////////////////////////// CREATE GAME OVER SCENE ///////////////////////////////////////
