1787b66e2d0120657b11f1b2f72b926eb5d4a4dd
Joao4569 <johnnyrod4569@gmail.com>
2023-08-21 10:44:21 +0000

    feat: Add created by and educational disclaimer to home page

--- src/main.js
+++ src/main.js
@@ -527,16 +527,6 @@ scene("game", () => {
         loop: true
     })
 
-
-    //////////////////////////////// Exit Game Key Press ///////////////////////////////////////////////
-
-
-    // Delete this section!! For testing purposes only
-
-    onKeyPress("q", () => {
-        go("over")
-    })
-
 })
 
 
